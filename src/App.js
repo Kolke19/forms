d78b391e85bca0cb200ef3585e1215cb510b02e6
Jorge gonzalez Notebook <jdgonzalez31@gmail.com>
2022-06-06 00:06:50 -0300

    trabajando con forms

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("kolke");
+
+  //comentario: crear la funcion setName..
+  // const handleOnChange = (e) => {
+  //   setName(e.target.value);
+  // };
+
+  const [lastName, setLastName] = useState("");
+
+  const [age, setAge] = useState(0);
+
+  // const handleAge = (e) => {
+  //   setAge(e.target.value);
+  // };
+
+  // const handleLastName = (e) => {
+  //   setLastName(e.target.value);
+  // };
+  //Simulando que enviamos los datos al backend
+  const handleSubmit = (e) => {
+    e.preventDefault(); //mirar bien como escribimos metodos, si no no funcionan
+    console.log("enviando...", { name, lastName, age });
+    setName(""); //aca seteamos para que el campo pase a estar vacio
+    setLastName(""); //limpiando el input de lastName
+    setAge(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>hola desde mi formulario</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Nombre</label>
+            <br />
+            <input
+              id="name"
+              type="text"
+              placeholder="Put your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <br />
+            <label>Apellido</label>
+            <br />
+            <input
+              id="lastName"
+              type="text"
+              placeholder="ingrese su apellido.."
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <br />
+            <label>Age</label>
+            <br />
+            <input
+              id="age"
+              type="number"
+              placeholder="put your age .."
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div></div>
+    </>
   );
 }
 
